6ccebcbbe5f89e8180821bab69795d6631feaa56
Joel Dalley <joeldalley@gmail.com>
2014-05-27 14:26:27 -0600

    add code to update db table with the sent timestamp

--- send_action_reminder.php
+++ send_action_reminder.php
@@ -12,6 +12,10 @@ instead just meant for educational purposes.
 
 */
 
+// Feel free to change this to match your location.
+// http://www.php.net/manual/en/timezones.america.php (Assuming America)
+date_default_timezone_set('America/Denver');
+
 // How long in between each action step reminder.
 define(INTERVAL, 86400*2); // 2 days, in seconds.
 
@@ -39,20 +43,42 @@ $when = time() - $which_step * INTERVAL;
 // given number, that haven't already been sent out.
 // NOTE: the date math can probably be done differently, 
 //       but for now, we stick to UNIX epochs.
-$query = "SELECT email_addr,$step_col_name FROM client_contact"
-       . " WHERE $step_date_col=0"
-       . "   AND UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP($when) "
-       . "       >= UNIX_TIMESTAMP(date_added)";
+$select_query = "SELECT email_addr,$step_col_name,date_added FROM client_contact"
+              . " WHERE $step_date_col=0"
+              . "   AND UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP($when) "
+              . "       >= UNIX_TIMESTAMP(date_added)";
 
 // Select matching rows.
 $link = new mysqli('localhost', 'contactor', '7sdfa3w8', 'test');
-$res = $link->query($query);
+$select_res = $link->query($select_query);
 
-// Iterate over rows & send email.
-while ($row = $res->fetch_array()) {
-    list($addr, $step) = $row;
 
-    // Uncomment to actually send an email.
-    // mail($addr, 'Action Step Reminder', "Your action step for today: $step.");
-    echo "Sent message to `$addr`, for action step `$which_step`, which was `$step`\n";
+// Iterate over rows, send email, and update the sent 
+// date column for the rows corresponding to the emails sent.
+while ($row = $select_res->fetch_array()) {
+    list($addr, $step_col_value, $date_added) = $row;
+
+    // Send the email, and print out that this happened. (Another approach
+    // instead of printing,  would be to blind carbon-copy yourself.)
+    mail($addr,                                           // Mail-to address.
+         "Action Step Reminder - Step $which_step",       // Subject.
+         "Your action step for today: $step_col_value."); // Body.
+    echo "Sent message to `$addr`, for action step `$which_step`,",
+         " which was `$step_col_value`\n";
+
+    // Record the timestamp that it was sent, so it won't get selected next time.
+    $update_time = date('Y-m-d H:i:s');
+    $update_query = "UPDATE client_contact SET $step_date_col=? "
+                  . " WHERE email_addr=? AND $step_col_name=? AND date_added=?";
+    $stmt = $link->prepare($update_query);
+    $stmt->bind_param('ssss', $update_time, $addr, $step_col_value, $date_added);
+    $update_res = $stmt->execute();
+
+    // WUT? (This is for debugging; get rid of this, for production.)
+    if (!$update_res) {
+        echo "UPDATE FAILED:\n\n";
+        var_dump($update_query);
+        var_dump($update_time, $addr, $step_col_value, $date_added);
+        exit;
+    }
 }
