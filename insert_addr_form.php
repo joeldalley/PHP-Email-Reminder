fc34742fdd025c8ce11f159e1ef5ea7c0287923c
Joel Dalley <joeldalley@gmail.com>
2014-06-08 10:38:20 -0600

    update html title

--- insert_addr_form.php
+++ insert_addr_form.php
@@ -28,7 +28,7 @@ if (!$mode) {
     <html>
       <head>
         <meta charset="utf-8"/>
-        <title>jQuery UI Datepicker - Default functionality</title>
+        <title>Action Steps Reminder Form</title>
         <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css"/>
         <script src="//code.jquery.com/jquery-1.10.2.js"></script>
         <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
